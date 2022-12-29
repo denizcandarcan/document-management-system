d32c26f19d56900e1be6a20336e3aeb3f63c829f
Denizcan Darcan <denizcandarcan@outlook.com>
2022-12-30 01:35:41 +0300

    Department and user operations added.

---
+++ DocumentManagementSystem.UI/Controllers/DepartmentController.cs
@@ -0,0 +1,57 @@
+﻿using DocumentManagementSystem.Business.Interfaces;
+using DocumentManagementSystem.Business.Services;
+using DocumentManagementSystem.Dtos;
+using DocumentManagementSystem.UI.Extensions;
+using DocumentManagementSystem.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace DocumentManagementSystem.UI.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDeparmentService _departmentService;
+
+        public DepartmentController(IDeparmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var departments = await _departmentService.GetAllAsync();
+            return this.ResponseView(departments);
+        }
+        public async Task<IActionResult> Update(int id)
+        {
+            var response = await _departmentService.GetByIdAsync<DepartmentUpdateDto>(id);
+            return this.ResponseView(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(DepartmentUpdateDto dto)
+        {
+            var response = await _departmentService.UpdateAsync(dto);
+            return this.ResponseRedirectAction(response,"Index");
+        }
+        public IActionResult Create()
+        {
+            var model = new DepartmentCreateDto() { };
+            return View("Create");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(DepartmentCreateDto dto)
+        {
+            if(dto.Definition == null)
+            {
+                return View("Create");
+            }
+            else
+            {
+                var createResponse = await _departmentService.CreateAsync(dto);
+                return this.ResponseRedirectAction(createResponse, "Index");
+            }
+        }
+    }
+}
---
+++ DocumentManagementSystem.UI/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using DocumentManagementSystem.Business.Interfaces;
+using DocumentManagementSystem.Business.Services;
+using DocumentManagementSystem.DataAccess.Contexts;
+using DocumentManagementSystem.Dtos;
+using DocumentManagementSystem.UI.Extensions;
+using DocumentManagementSystem.UI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentManagementSystem.UI.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IAppUserService _appUserService;
+        private readonly DocumentContext _context;
+        private readonly IMapper _mapper;
+        private readonly IDeparmentService _departmentService;
+
+
+        public UserController(IAppUserService appUserService, DocumentContext context, IMapper mapper, IDeparmentService departmentService)
+        {
+            _appUserService = appUserService;
+            _context = context;
+            _mapper = mapper;
+            _departmentService = departmentService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _appUserService.GetAllAsync();
+            return this.ResponseView(users);
+        }
+        public async Task<IActionResult> Update(int id)
+        {
+            var user = _context.AppUsers.FirstOrDefault(x => x.Id == id);
+            if (user != null)
+            {
+                var userDto = _mapper.Map<AppUserUpdateDto>(user);
+                var model = _mapper.Map<UserUpdateModel>(userDto);
+                var response = await _departmentService.GetAllAsync();
+                model.Departments = new SelectList(response.Data, "Id", "Definition");
+                return View(model);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(UserUpdateModel model)
+        {
+            var dto = _mapper.Map<AppUserUpdateDto>(model);
+            await _appUserService.UpdateAsync(dto);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
